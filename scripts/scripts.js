d32f6df76792ac4c0dc5e223c79e9b6bb5f6032a
Frankie Smith <frankie@5hdagency.com>
2020-10-31 00:38:50 -0400

    add swiper

---
+++ scripts/scripts.js
@@ -0,0 +1,22 @@
+	// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+//Swiper Settings
+
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+})
